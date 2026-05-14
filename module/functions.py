09afd47f03f73305540630b56316496a4b0b187c
bishu-ebl <bishu.ebl@gmail.com>
2026-05-14 14:37:34 +0600

    Implement add,edit button

--- module/functions.py
+++ module/functions.py
@@ -2,15 +2,16 @@
 #     with open('Files/todos.txt', 'r') as file_local:
 #         todos_local = file_local.readlines()
 #     return todos_local
+FILEPATH = "Files/todos.txt"
 
 # def get_todos(filepath"):
-def get_todos(filepath="Files/todos.txt"):
+def get_todos(filepath=FILEPATH):
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 # def write_todos(filepath, file_arg):
-def write_todos(file_arg,filepath="Files/todos.txt"):
+def write_todos(file_arg,filepath=FILEPATH):
     with open(filepath, 'w') as file:
         file.writelines(file_arg)
 
